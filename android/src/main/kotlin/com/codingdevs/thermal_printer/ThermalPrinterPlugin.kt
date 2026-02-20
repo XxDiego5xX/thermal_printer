c24d08deaad0af83f206eb17fc56ec6f2192593a
΅Diego¨ <samanodiego888@gmail.com>
2026-02-20 14:28:39 -0600

    Corrección de ThermalPrinterPlugin.kt 6

--- android/src/main/kotlin/com/codingdevs/thermal_printer/ThermalPrinterPlugin.kt
+++ android/src/main/kotlin/com/codingdevs/thermal_printer/ThermalPrinterPlugin.kt
@@ -178,8 +178,7 @@ class ThermalPrinterPlugin : FlutterPlugin,
         }
 
         val notGranted = permissions.filter {
-            ActivityCompat.checkSelfPermission(ctx, it)
-            != PackageManager.PERMISSION_GRANTED
+            ActivityCompat.checkSelfPermission(ctx, it) != PackageManager.PERMISSION_GRANTED
         }
 
         if (notGranted.isNotEmpty()) {
